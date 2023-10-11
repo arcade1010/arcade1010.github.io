c78e9358187637f7e582a9b8733020571188ca21
arcade1010 <enwurster@gmail.com>
2023-10-11 18:55:30 -0400

    continued firstscripts

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,3 +1,17 @@
 function scriptTest() {
     alert("Hey my script is running");
 }
+
+function displayName() {
+    let x = document.getElementById("frm1");
+    let text1 = "The Elegant Whale welcomes you, ";
+    for (let i=0; i<x.length; i++) {
+        text1 += x.elements[i].value;
+    }
+    x = document.getElementById("frm2");
+    let text2 = ". We're glad you are doing ";
+    for (let i=0; i<x.length; i++) {
+        text2 += x.elements[i].value;
+    }
+    document.getElementById("displayNameFeeling").innerHTML = (text1 + text2 + "!");
+}
