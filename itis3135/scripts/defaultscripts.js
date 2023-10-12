d4c531dcbd57ac05ba8928e20f41f5770f841a35
arcade1010 <enwurster@gmail.com>
2023-10-12 10:59:34 -0400

    finished firstScripts

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -53,3 +53,32 @@ function favNum() {
     }
     }
 }
+
+function howManyWhales() {
+    document.getElementById("displayHowManyWhales").innerHTML = "There are currently over 1.5 MILLION whales in the world";
+}
+function howManySpecies() {
+    document.getElementById("displaySpecies").innerHTML = "There are a total of 77 species of whales with 10 grouped famillies"
+}
+function randomWhaleFact() {
+    let x = Math.round(Math.random()*5);
+    let message = "";
+    switch (x) {
+        case 0: message = "Male humpback whales found in U.S. waters sing complex songs in winter breeding areas in waters near Hawaii, in the Caribbean, and elsewhere that can last up to 20 minutes and be heard miles away.";
+        break;
+        case 1: message = "The blue whale is the largest animal that ever lived and can grow to 90 or more feet and weigh as much as 24 elephants! That’s more than 330,000 pounds ";
+        break;
+        case 2: message = "Some species of whales are among the longest lived mammals. Scientists estimate bowhead whales (a baleen whale found in the Arctic) can live for more than 200 years, and killer whales (a toothed whale found in various habitats worldwide) can live for more than 100 years.";
+        break;
+        case 3: message = "Killer whales are highly social and often travel in groups that are matrifocal—a family unit focused or centered on the mother.";
+        break;
+        case 4: message = "Gray whales make one of the longest annual migrations of any mammal: they travel about 10,000 miles (16,000 km) round trip!";
+        break;
+        default:
+                message = "Press it again";
+    }
+    document.getElementById("displayFact").innerHTML = message;
+}
+function whaleJoke() {
+    document.getElementById("displayWhaleJoke").innerHTML = "Free Willy!";
+}
