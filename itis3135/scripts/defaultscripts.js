7fbc0e010ce70f121e54e6da5d5ceebe1a5f36dd
arcade1010 <enwurster@gmail.com>
2023-10-11 21:38:01 -0400

    continued working on firstscripts, finished polygon section

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -15,3 +15,41 @@ function displayName() {
     }
     document.getElementById("displayNameFeeling").innerHTML = (text1 + text2 + "!");
 }
+
+function favNum() {
+    let x = document.getElementById("favNumInput").value;
+    x = Math.round(x);
+    
+
+    if (isNaN(x)){
+        document.getElementById("displayNumber").innerHTML = "number is coming through as NaN";
+    }
+    else {
+    switch (x) {
+        case 1: document.getElementById("displayNumber").innerHTML = "heisagon";
+        break;
+        case 2: document.getElementById("displayNumber").innerHTML = "digon";
+        break;
+        case 3: document.getElementById("displayNumber").innerHTML = "treisagon";
+        break;
+        case 4: document.getElementById("displayNumber").innerHTML = "tettaresagon";
+        break;
+        case 5: document.getElementById("displayNumber").innerHTML = "pentegon";
+        break;
+        case 6: document.getElementById("displayNumber").innerHTML = "hexagon";
+        break;
+        case 7: document.getElementById("displayNumber").innerHTML = "heptagon";
+        break;
+        case 8: document.getElementById("displayNumber").innerHTML = "oktogon";
+        break;
+        case 9: document.getElementById("displayNumber").innerHTML = "enneagon";
+        break;
+        case 10: document.getElementById("displayNumber").innerHTML = "dekagon";
+        break;
+        case 0: document.getElementById("displayNumber").innerHTML = "Not a polygon (or a shape for that matter)";
+        break;
+        default: 
+            document.getElementById("displayNumber").innerHTML = "Please enter a number from 1 to 10";
+    }
+    }
+}
