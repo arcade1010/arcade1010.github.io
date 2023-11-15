2370105c33f7c2d3ac86fc2fb425ea98ec8608bc
arcade1010 <enwurster@gmail.com>
2023-11-15 18:37:37 -0500

    got scrolling and nav to work

---
+++ enWurster/scripts/general.js
@@ -0,0 +1,15 @@
+function educationScroll() {
+    document.getElementById("education").scrollIntoView();
+}
+function projectsScroll() {
+    document.getElementById("projects").scrollIntoView();
+}
+function aboutMeScroll() {
+    document.getElementById("aboutMe").scrollIntoView();
+}
+function myPCScroll() {
+    document.getElementById("myPC").scrollIntoView();
+}
+function contactScroll() {
+    document.getElementById("contact").scrollIntoView();
+}
