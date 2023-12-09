4976381a4d1d25e27ce98aff722050ec33f96974
arcade1010 <enwurster@gmail.com>
2023-12-09 14:31:13 -0500

    started cpp5

--- itis3135/clientProject2/scripts/clientProjectPart2.js
+++ itis3135/clientProject2/scripts/clientProjectPart2.js
@@ -2,3 +2,20 @@ function emailSubmit() {
     let email = document.getElementById('email').value;
     document.getElementById("emailResult").innerHTML = "We will contact you at " + email + " in approximately 3 tp 5 business days";
 }
+
+// JavaScript for Image Slider
+const slider = document.querySelector('.slider');
+let counter = 0;
+
+function nextSlide() {
+   counter++;
+   if (counter === slider.children.length) counter = 0;
+   updateSlider();
+}
+
+function updateSlider() {
+   const transformValue = -100 * counter + '%';
+   slider.style.transform = 'translateX(' + transformValue + ')';
+}
+
+setInterval(nextSlide, 3000);
