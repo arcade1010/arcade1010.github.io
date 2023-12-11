6ede5fa90a92c599333c26a549f7e83fe3ce9f93
arcade1010 <enwurster@gmail.com>
2023-12-10 21:51:45 -0500

    sdf

--- itis3135/clientProject2/scripts/clientProjectPart2.js
+++ itis3135/clientProject2/scripts/clientProjectPart2.js
@@ -16,15 +16,33 @@ myForm.addEventListener('submit', function (event) {
 
 
 let slideshow = ['<img src="images/debrisRemoval.png" id="image-in-slide"><figcaption id="caption-of-image">Debris clearing and removal - Summer 2023</figcaption>',
-'<img src="images/debrisRemoval2.png" id="image-in-slide"><figcaption id="caption-of-image">"Debris/material moving - Summer 2022"</figcaption>',
-'<img src="images/garden.png" id="image-in-slide"><figcaption id="caption-of-image">"Garden organization and masonry- Summer 2022"</figcaption>',
-'<img src="images/fence.jpg" id="image-in-slide"><figcaption id="caption-of-image">"10ft fence constructions - Spring 2023"</figcaption>']
-let images = ["images/debrisRemoval.png", "images/debrisRemoval2.png", "images/garden.png", "images/fence.jpg"];
-let captions = ["Debris clearing and removal - Summer 2023", "Debris/material moving - Summer 2022",
-                "Garden organization and masonry- Summer 2022", "10ft fence constructions - Spring 2023"];
+'<img src="images/debrisRemoval2.png" id="image-in-slide"><figcaption id="caption-of-image">Debris/material moving - Summer 2022</figcaption>',
+'<img src="images/garden.png" id="image-in-slide"><figcaption id="caption-of-image">Garden organization and masonry- Summer 2022</figcaption>',
+'<img src="images/fence.jpg" id="image-in-slide"><figcaption id="caption-of-image">10ft fence constructions - Spring 2023</figcaption>']
+
+let currentSlideIndex = 0;
+
+
+function showCurrentSlide() {
+  document.getElementById("slide-figure").innerHTML = slideshow[currentSlideIndex];
+}
 
 function nextSlide() {
-  for (let i=0; i<slideshow.length()-1; i++) {
+  currentSlideIndex = (currentSlideIndex + 1) % slideshow.length;
+  showCurrentSlide();
+}
+
+function prevSlide() {
+  currentSlideIndex = (currentSlideIndex - 1 + slideshow.length) % slideshow.length;
+  showCurrentSlide();
+}
+
+// Initialize the slide display
+showCurrentSlide();
+
+
+/*function nextSlide() {
+  for (let i=0; i<slideshow.length-1; i++) {
     if(document.getElementById("slide-figure").innerHTML == slideshow[i]){
       document.getElementById("slide-figure").innerHTML = slideshow[i+1];
     }
@@ -32,10 +50,11 @@ function nextSlide() {
   document.getElementById("slide-figure").innerHTML = slideshow[0];
 }
 function prevSlide() {
-  for (let i=1; i<slideshow.length(); i++) {
+  for (let i=1; i<slideshow.length; i++) {
     if(document.getElementById("slide-figure").innerHTML == slideshow[i]){
       document.getElementById("slide-figure").innerHTML = slideshow[i-1];
     }
   }
-  document.getElementById("slide-figure").innerHTML = slideshow[slideshow.length()-1];
+  document.getElementById("slide-figure").innerHTML = slideshow[slideshow.length-1];
 }
+*/
