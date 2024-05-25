82b6ce0be6639a330f66b722f191594d824dfd6a
arcade1010 <enwurster@gmail.com>
2024-05-24 22:19:19 -0400

    tryin out java stuff ig

---
+++ project_unamed/justTryingOut.java
@@ -0,0 +1,9 @@
+public class justTryingOut {
+    
+    public void justTryingOut(int n){
+        String message = "HellllloooooooooWooooooooorrrllldd!!";
+        for(int i=0; i<n; i++){
+            System.out.println(message);
+        }
+    }
+}
