84856a41076f07d6cf8eb851f26125a7c4a54ced
arcade1010 <enwurster@gmail.com>
2024-06-11 17:59:15 -0400

    working simple user input

--- project_unamed/Main.java
+++ project_unamed/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+        //New jTO object
         justTryingOut jTO = new justTryingOut();
-        jTO.justTryingOut(4);
-    }
+        
+        //New Scanner object for user input
+        Scanner sc = new Scanner(System.in);
+        String input = "";
 
+        //print hello, bye, or nothing from input, loop until user says stop
+         while (!input.equalsIgnoreCase("stop")) {
+            System.out.println("1. print Hi, 2. print bye, 3. print nothing ig. say stop to stop");
+            input = jTO.speak(sc.next());
+        } 
+        //close the scanner
+        sc.close();
+    
+    }
 }
--- project_unamed/justTryingOut.java
+++ project_unamed/justTryingOut.java
@@ -1,9 +1,45 @@
 public class justTryingOut {
-    
-    public void justTryingOut(int n){
-        String message = "HellllloooooooooWooooooooorrrllldd!!";
-        for(int i=0; i<n; i++){
-            System.out.println(message);
-        }
+    private String hello;
+    private String bye;
+    private String nothing;
+
+    public justTryingOut() {
+        this.hello = "Hello";
+        this.bye = "Bye";
+        this.nothing = "Northing";
     }
+
+    public String speak(String s){
+                   
+            switch (s) {
+                case "stop":
+                    System.out.println("Stopping");
+                    return s;
+                case "1":
+                    sayHello();
+                    return s;
+                case "2":
+                    sayBye();
+                    return s;
+                case "3":
+                    sayNothing();
+                    return s;
+                
+                default:
+                    return s;
+            } 
+        }
+    
+
+    public void sayHello(){
+        System.out.println(hello);
+    }
+    public void sayBye(){
+        System.out.println(bye);
+    }
+    public void sayNothing(){
+        System.out.println(nothing);
+    }
+
+   
 }
